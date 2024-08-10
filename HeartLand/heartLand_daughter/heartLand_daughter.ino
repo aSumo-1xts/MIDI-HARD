7794c8041dcd2aca3d6e3c9f0fc6b623da921470
ｱｽﾓ <1xtelescope@gmail.com>
2024-08-11 01:49:41 +0900

    fix: CC Numberの書き方を変更

--- HeartLand/heartLand_daughter/heartLand_daughter.ino
+++ HeartLand/heartLand_daughter/heartLand_daughter.ino
@@ -8,8 +8,8 @@
  * @details     Just send serial MIDI message from Serial1 to "mother"
  */
 
-#include <Control_Surface.h> //!< https://github.com/tttapa/Control-Surface.git
-
+//! https://github.com/tttapa/Control-Surface.git
+#include <Control_Surface.h>
 //! 単純なシリアル通信のためのインスタンス
 SerialMIDI_Interface<decltype(Serial1)> midi_ser {Serial1, MIDI_BAUD};
 
@@ -21,61 +21,64 @@ uint8_t enc_multi   = 5;            //!< 汎用エンコーダの係数
 
 /* ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑ variable to change ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑ */
 
+
+
 uint8_t util01 = 2;     //!< 余りその1
 uint8_t util02 = 3;     //!< 余りその2
 
 
 
 CCAbsoluteEncoder enc[] = {
-    {{26, 28}, {MIDI_CC::Sound_Controller_1, Channel::createChannel(ch[0])}, BPM_multi, }, // enc01
-    {{29, 31}, {MIDI_CC::Sound_Controller_2, Channel::createChannel(ch[0])}, enc_multi, }, // enc02
-    {{32, 34}, {MIDI_CC::Sound_Controller_3, Channel::createChannel(ch[0])}, enc_multi, }, // enc03
-    {{35, 37}, {MIDI_CC::Sound_Controller_4, Channel::createChannel(ch[0])}, enc_multi, }, // enc04
+    {{26, 28}, {3, Channel::createChannel(ch[0])}, BPM_multi, }, // enc01
+    {{29, 31}, {4, Channel::createChannel(ch[0])}, enc_multi, }, // enc02
+    {{32, 34}, {5, Channel::createChannel(ch[0])}, enc_multi, }, // enc03
+    {{35, 37}, {6, Channel::createChannel(ch[0])}, enc_multi, }, // enc04
 };
 
 
 
 CCButton sw[] = {
-    {27, {MIDI_CC::Sound_Controller_5, Channel::createChannel(ch[0])}, }, // enc_sw01
-    {30, {MIDI_CC::Sound_Controller_6, Channel::createChannel(ch[0])}, }, // enc_sw02
-    {33, {MIDI_CC::Sound_Controller_7, Channel::createChannel(ch[0])}, }, // enc_sw03
-    {36, {MIDI_CC::Sound_Controller_8, Channel::createChannel(ch[0])}, }, // enc_sw04
+    {27, {7,  Channel::createChannel(ch[0])}, }, // enc_sw01
+    {30, {8,  Channel::createChannel(ch[0])}, }, // enc_sw02
+    {33, {9,  Channel::createChannel(ch[0])}, }, // enc_sw03
+    {36, {10, Channel::createChannel(ch[0])}, }, // enc_sw04
 
-    {4,  {MIDI_CC::General_Purpose_Controller_1, Channel::createChannel(ch[0])}, }, // ary01
-    {5,  {MIDI_CC::General_Purpose_Controller_2, Channel::createChannel(ch[0])}, }, // ary02
-    {6,  {MIDI_CC::General_Purpose_Controller_3, Channel::createChannel(ch[0])}, }, // ary03
-    {7,  {MIDI_CC::General_Purpose_Controller_4, Channel::createChannel(ch[0])}, }, // ary04
-    {8,  {MIDI_CC::General_Purpose_Controller_5, Channel::createChannel(ch[0])}, }, // ary05
-    {9,  {MIDI_CC::General_Purpose_Controller_6, Channel::createChannel(ch[0])}, }, // ary06
-    {10, {MIDI_CC::General_Purpose_Controller_7, Channel::createChannel(ch[0])}, }, // ary07
-    {11, {MIDI_CC::General_Purpose_Controller_8, Channel::createChannel(ch[0])}, }, // ary08
+    {4,  {11, Channel::createChannel(ch[0])}, }, // ary01
+    {5,  {12, Channel::createChannel(ch[0])}, }, // ary02
+    {6,  {13, Channel::createChannel(ch[0])}, }, // ary03
+    {7,  {14, Channel::createChannel(ch[0])}, }, // ary04
+    {8,  {15, Channel::createChannel(ch[0])}, }, // ary05
+    {9,  {16, Channel::createChannel(ch[0])}, }, // ary06
+    {10, {17, Channel::createChannel(ch[0])}, }, // ary07
+    {11, {18, Channel::createChannel(ch[0])}, }, // ary08
 
-    {22, {MIDI_CC::Sound_Controller_5, Channel::createChannel(ch[1])}, }, // Sw01
-    {23, {MIDI_CC::Sound_Controller_6, Channel::createChannel(ch[1])}, }, // Sw02
-    {24, {MIDI_CC::Sound_Controller_5, Channel::createChannel(ch[2])}, }, // Sw03
-    {25, {MIDI_CC::Sound_Controller_6, Channel::createChannel(ch[2])}, }, // Sw04
+    {22, {6,  Channel::createChannel(ch[1])}, }, // Sw01
+    {23, {7,  Channel::createChannel(ch[1])}, }, // Sw02
+    
+    {24, {6,  Channel::createChannel(ch[2])}, }, // Sw03
+    {25, {7,  Channel::createChannel(ch[2])}, }, // Sw04
 };
 
 
 
 CCPotentiometer pot[] = {
-    {A0,  {MIDI_CC::Sound_Controller_3,  Channel::createChannel(ch[1])}, }, // pot01_01
-    {A1,  {MIDI_CC::Sound_Controller_4,  Channel::createChannel(ch[1])}, }, // pot01_02
-    {A2,  {MIDI_CC::Sound_Controller_5,  Channel::createChannel(ch[1])}, }, // pot01_03
-    {A3,  {MIDI_CC::Sound_Controller_6,  Channel::createChannel(ch[1])}, }, // pot01_04
-    {A4,  {MIDI_CC::Sound_Controller_7,  Channel::createChannel(ch[1])}, }, // pot02_01
-    {A5,  {MIDI_CC::Sound_Controller_8,  Channel::createChannel(ch[1])}, }, // pot02_02
-    {A6,  {MIDI_CC::Sound_Controller_9,  Channel::createChannel(ch[1])}, }, // pot02_03
-    {A7,  {MIDI_CC::Sound_Controller_10, Channel::createChannel(ch[1])}, }, // pot02_04
+    {A0,  {8,  Channel::createChannel(ch[1])}, }, // pot01_01
+    {A1,  {9,  Channel::createChannel(ch[1])}, }, // pot01_02
+    {A2,  {10, Channel::createChannel(ch[1])}, }, // pot01_03
+    {A3,  {11, Channel::createChannel(ch[1])}, }, // pot01_04
+    {A4,  {12, Channel::createChannel(ch[1])}, }, // pot02_01
+    {A5,  {13, Channel::createChannel(ch[1])}, }, // pot02_02
+    {A6,  {14, Channel::createChannel(ch[1])}, }, // pot02_03
+    {A7,  {15, Channel::createChannel(ch[1])}, }, // pot02_04
 
-    {A8,  {MIDI_CC::Sound_Controller_3,  Channel::createChannel(ch[2])}, }, // pot03_01
-    {A9,  {MIDI_CC::Sound_Controller_4,  Channel::createChannel(ch[2])}, }, // pot03_02
-    {A10, {MIDI_CC::Sound_Controller_5,  Channel::createChannel(ch[2])}, }, // pot03_03
-    {A11, {MIDI_CC::Sound_Controller_6,  Channel::createChannel(ch[2])}, }, // pot03_04
-    {A12, {MIDI_CC::Sound_Controller_7,  Channel::createChannel(ch[2])}, }, // pot04_01
-    {A13, {MIDI_CC::Sound_Controller_8,  Channel::createChannel(ch[2])}, }, // pot04_02
-    {A14, {MIDI_CC::Sound_Controller_9,  Channel::createChannel(ch[2])}, }, // pot04_03
-    {A15, {MIDI_CC::Sound_Controller_10, Channel::createChannel(ch[2])}, }, // pot04_04
+    {A8,  {8,  Channel::createChannel(ch[2])}, }, // pot03_01
+    {A9,  {9,  Channel::createChannel(ch[2])}, }, // pot03_02
+    {A10, {10, Channel::createChannel(ch[2])}, }, // pot03_03
+    {A11, {11, Channel::createChannel(ch[2])}, }, // pot03_04
+    {A12, {12, Channel::createChannel(ch[2])}, }, // pot04_01
+    {A13, {13, Channel::createChannel(ch[2])}, }, // pot04_02
+    {A14, {14, Channel::createChannel(ch[2])}, }, // pot04_03
+    {A15, {15, Channel::createChannel(ch[2])}, }, // pot04_04
 };
 
 
@@ -84,7 +87,7 @@ CCPotentiometer pot[] = {
 void setup() {
     pinMode(util01, INPUT_PULLUP);
     pinMode(util02, INPUT_PULLUP);
-
+    
     Control_Surface.begin();
 }
 
--- HeartLand/heartLand_mother/heartLand_mother.ino
+++ HeartLand/heartLand_mother/heartLand_mother.ino
@@ -8,11 +8,14 @@
  * @details     Merge MIDI message; "daughter"s one and mine, and TX/RX with USB
  */
 
-#include <Control_Surface.h>    //!< https://github.com/tttapa/Control-Surface.git
-
-USBMIDI_Interface midi_usb;                     //!< Instantiate a MIDI over USB interface
-HardwareSerialMIDI_Interface midi_ser {Serial1};//!< Instantiate a 5-pin DIN MIDI interface
-BidirectionalMIDI_Pipe pipes;                   //!< Instantiate the pipe to connect the two interfaces
+//! https://github.com/tttapa/Control-Surface.git
+#include <Control_Surface.h>
+//! Instantiate a MIDI over USB interface
+USBMIDI_Interface midi_usb;
+//! Instantiate a 5-pin DIN MIDI interface
+HardwareSerialMIDI_Interface midi_ser {Serial1, MIDI_BAUD};
+//! Create a MIDI pipe factory with 3 pipes
+BidirectionalMIDI_PipeFactory<3> pipes;
 
 /* ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ variable to change ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ */
 
@@ -22,37 +25,46 @@ uint8_t maxPWM  = 255;          //!< LED最明値
 
 /* ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑ variable to change ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑ */
 
+
+
 uint8_t LEDpin[2] = {3, 5};     //!< indicator
 
 
 
 CCButton sw[] = {
-    {10,  {MIDI_CC::Sound_Controller_1, Channel::createChannel(ch[1])}, }, // Solo01
-    {14,  {MIDI_CC::Sound_Controller_2, Channel::createChannel(ch[1])}, }, // Solo02
-    {15,  {MIDI_CC::Sound_Controller_1, Channel::createChannel(ch[2])}, }, // Solo03
-    {16,  {MIDI_CC::Sound_Controller_2, Channel::createChannel(ch[2])}, }, // Solo04
+    {10,  {1, Channel::createChannel(ch[1])}, }, // Solo01
+    {14,  {2, Channel::createChannel(ch[1])}, }, // Solo02
+
+    {15,  {1, Channel::createChannel(ch[2])}, }, // Solo03
+    {16,  {2, Channel::createChannel(ch[2])}, }, // Solo04
 };
 
 
 
 CCPotentiometer pot[] = {
-    {A0, {MIDI_CC::Sound_Controller_3,  Channel::createChannel(ch[1])}, }, // Vol01
-    {A1, {MIDI_CC::Sound_Controller_4,  Channel::createChannel(ch[1])}, }, // Vol02
-    {A2, {MIDI_CC::Sound_Controller_3,  Channel::createChannel(ch[2])}, }, // Vol03
-    {A3, {MIDI_CC::Sound_Controller_4,  Channel::createChannel(ch[2])}, }, // Vol04
+    {A8, {1, Channel::createChannel(ch[0])}, }, // A x B
+    {A9, {2, Channel::createChannel(ch[0])}, }, // Master
 
-    {A8, {MIDI_CC::Balance,         Channel::createChannel(ch[0])}, }, // A x B
-    {A9, {MIDI_CC::Channel_Volume,  Channel::createChannel(ch[0])}, }, // Master
+    {A0, {3, Channel::createChannel(ch[1])}, }, // Vol01
+    {A1, {4, Channel::createChannel(ch[1])}, }, // Vol02
+    {A6, {5, Channel::createChannel(ch[1])}, }, // FX_A
+
+    {A2, {3, Channel::createChannel(ch[2])}, }, // Vol03
+    {A3, {4, Channel::createChannel(ch[2])}, }, // Vol04
+    {A7, {5, Channel::createChannel(ch[2])}, }, // FX_B
 };
 
 
 
 //! @brief setup関数
 void setup() {
-    Control_Surface.begin();
-
+    Control_Surface | pipes | midi_usb;
+    Control_Surface | pipes | midi_ser;
+    
     midi_ser | pipes | midi_usb;    // Manually route Serial to USB / USB to Serial
     MIDI_Interface::beginAll();     // Initialize the MIDI interfaces
+
+    Control_Surface.begin();
 
     Control_Surface.setMIDIInputCallbacks(nullptr, nullptr, nullptr, realTimeMessageCallback);
 }
@@ -68,19 +80,19 @@ float     BPM       = 0;  //!< グローバルBPM
 uint8_t   ppqn      = 0;  //!< 24 Pulses Per Quarter Note
 uint32_t  startTime = 0;  //!< カウント開始時刻
 /**
- * @brief         なんかよく解らんけどsetHandleClockの代わり！使用中はシリアルモニタを絶対に開かないこと
- * @param rt      これも謎パラメータ
+ * @brief       なんかよく解らんけどsetHandleClockの代わり！使用中はシリアルモニタを絶対に開かないこと
+ * @param rt    これも謎パラメータ
  */
 bool realTimeMessageCallback(RealTimeMessage rt) {
-    float   preBPM = 0;   // 一旦の計算結果としてのBPM 
+    float preBPM = 0;   // 一旦の計算結果としてのBPM 
 
-    if (ppqn == 0) {
+    if(ppqn == 0) {
         startTime = micros();
         analogWrite(LEDpin[0], minPWM);
     }
     ppqn++;             // カウントアップ
 
-    if (ppqn > 24) {    // 24回＝1拍
+    if(ppqn > 24) {     // 24回＝1拍
         preBPM  = 6.0e+07 / float(micros() - startTime); 
 
         if(20 <= preBPM && preBPM <= 999) {
